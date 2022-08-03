bd2335291c7f4c853324703a16f330dc5edbdde5
Domingo John Ruselle R – MBTC Contractual <t-jrdomingo@mbtc.mgc.local>
2022-08-03 17:47:49 +0800

    add all

---
+++ src/test/java/spring/javamvnbankapi/JavamvnBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.javamvnbankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavamvnBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
